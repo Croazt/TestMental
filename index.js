72c6af189754f685a48e359d747e3171a4b1329a
Croazt <fachry.croazt@gmail.com>
2021-01-11 21:26:04 +0700

    fix glitch bug

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var articleTitle = ["judul 1","judul 2","judul 3","judul 4"]
 var articleImage = ["images/215112.jpg","images/215112.jpg","images/215112.jpg","images/215112.jpg"]
 var articleDesc = ["INI DESKRIPSI SINGKAT ARTIKELNYA","INI DESKRIPSI SINGKAT ARTIKELNYA","INI DESKRIPSI SINGKAT ARTIKELNYA","INI DESKRIPSI SINGKAT ARTIKELNYA"]
-var articleDescColor = ["#a88d6f","#f6f5f1","#9aa5a7","#f6f5f1"]
-var articleTitleColor = ["#9aa5a7","#000000","#a88d6f","#9aa5a7"]
+var articleDescColor = ["#394867","#f1f6f9","#9ba4b4","#f1f6f9"]
+var articleTitleColor = ["#9ba4b4","#000000","#394867","#9ba4b4"]
+var articleDescFontColor = ["#f1f6f9","#394867","#14274e","#394867",]
+var articleTitleFontColor = ["#394867","#f1f6f9","#f1f6f9","#394867",]
 var column = document.querySelector(".column")
 for (let i = 0; i < 4; i++) {
-    column.innerHTML += '<div class="articles"><div class="article-container" style="background-color:'+articleDescColor[i]+';  ">'+articleDesc[i]+'</div><img src="'+articleImage[i]+'"><div class="article-title" style="background-color:'+articleTitleColor[i]+';"><p>'+articleTitle[i]+'</p></div></div>'    
+    column.innerHTML += '<div class="articles"><div class="article-container" style="background-color:'+articleDescColor[i]+';color:'+articleDescFontColor[i]+';  ">'+articleDesc[i]+'</div><img src="'+articleImage[i]+'"><div class="article-title" style="background-color:'+articleTitleColor[i]+';color:'+articleTitleFontColor[i]+'; "><p>'+articleTitle[i]+'</p></div></div>'    
 }
 
 window.addEventListener("scroll", function(){
